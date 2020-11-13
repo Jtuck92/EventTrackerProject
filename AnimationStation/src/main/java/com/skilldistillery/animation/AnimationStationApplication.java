25239b995eba5c17589a3a597019e0e67794d74e
Jess Tucker <tuckerjess92@gmail.com>
2020-11-13 17:42:00 -0600

    Stub Boot project with JPA project dependency

---
+++ AnimationStation/src/main/java/com/skilldistillery/animation/AnimationStationApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.animation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimationStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimationStationApplication.class, args);
+	}
+
+}
---
+++ AnimationStation/src/test/java/com/skilldistillery/animation/AnimationStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.animation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimationStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
