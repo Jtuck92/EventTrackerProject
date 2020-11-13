fa582998972dd6e44b58f8a86f61419ddc52730c
Jess Tucker <tuckerjess92@gmail.com>
2020-11-13 17:11:08 -0600

    Validate JPA/Hibernate config

--- JPAAnimation/src/main/java/com/skilldistillery/animation/entities/AnimatedFeature.java
+++ JPAAnimation/src/main/java/com/skilldistillery/animation/entities/AnimatedFeature.java
@@ -1,5 +1,72 @@
 package com.skilldistillery.animation.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "animated_feature")
 public class AnimatedFeature {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public AnimatedFeature() {
+		super();
+	}
+
+	public AnimatedFeature(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "AnimatedFeature [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnimatedFeature other = (AnimatedFeature) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 }
