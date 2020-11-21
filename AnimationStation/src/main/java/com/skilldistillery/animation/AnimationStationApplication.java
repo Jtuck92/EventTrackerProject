491f4f57c2942b4a6db905f9a832e6aff5411bca
Jess Tucker <tuckerjess92@gmail.com>
2020-11-20 18:37:13 -0600

    Front-end stubbed out.

--- AnimationStation/src/main/java/com/skilldistillery/animation/AnimationStationApplication.java
+++ AnimationStation/src/main/java/com/skilldistillery/animation/AnimationStationApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.animation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnimationStationApplication {
+public class AnimationStationApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AnimationStationApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimationStationApplication.class, args);
